920440de410cefb73f0686276409078fc0c91027
Evgeniy3 <90893731+Evgeniy3@users.noreply.github.com>
2022-09-15 17:12:20 +0500

    fixed files

--- src/view.js
+++ src/view.js
@@ -46,12 +46,14 @@ const handleModal = (elements, value) => {
 
 const showPosts = (elements, value, i18) => {
   const posts = value;
-  // console.log(postsState);
+  // const [uiState, ...posts] = value;
   const preparePosts = posts.map((post) => {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const href = document.createElement('a');
     // const typeOfTextPost = uiState.includes(post.id) ? 'fw-normal' : 'fw-bold';
+    // href.classList.add(typeOfTextPost);
+    href.classList.add('fw-bold');
     href.classList.add('fw-bold');
     href.setAttribute('data-id', `${post.id}`);
     href.setAttribute('href', `${post.link}`);
